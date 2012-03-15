3d90b0401126b2cfe52c710f5570f5bd36ac9815
coeio <coeio@62d9fa40-7f12-0410-8967-a910f1bb1c9e>
2012-03-15 00:37:53 +0000

    problem 9 solved
    git-svn-id: https://svn.inf.ufsc.br/coeio/projecteuller@812 62d9fa40-7f12-0410-8967-a910f1bb1c9e

--- problem9.cpp
+++ problem9.cpp
@@ -1,25 +1,21 @@
 #include <iostream>
+#include <cmath>
 //http://en.wikipedia.org/wiki/Formulas_for_generating_Pythagorean_triples
-
-bool isPythagoreanTriple(const int& a, const int& b, const int& c)
-{
-    if(!(a < b < c))
-        return false;
-
-    return (a*a + b*b == c*c);
-}
 
 int main()
 {
-    int a = 3;
-    int b = 4;
-    int c = 5;
-
-    if(isPythagoreanTriple(a,b,c))
-    {
-        std::cout << (a+b+c) << std::endl;
-        std::cout << (a*b*c) << std::endl;
-    }
-
+    for(int a = 0; a < 500; ++a)
+        for(int b = 0; b < 500; ++b)
+        {
+            int candidate = a*a + b*b;
+            double square_root = sqrt(candidate);
+            double fract, intpart;
+            fract = modf(square_root, &intpart);
+            if(!(fract > 0.0))
+                if(a + b + square_root == 1000){
+                    std::cout << a << ", " << b << ", " << square_root << std::endl;
+                    std::cout << (a * b * static_cast<int>(square_root)) << std::endl;
+                }
+        }
     return 0;
 }
