9bbf958c4597ed85d686bd31000cf8e980ca26e1
coeio <coeio@62d9fa40-7f12-0410-8967-a910f1bb1c9e>
2012-03-15 01:25:34 +0000

    trying to solve problem10, just a scratch
    git-svn-id: https://svn.inf.ufsc.br/coeio/projecteuller@814 62d9fa40-7f12-0410-8967-a910f1bb1c9e

--- problem10.cpp
+++ problem10.cpp
@@ -3,22 +3,32 @@
 
 int main()
 {
-    int max = 2000000;
+    std::vector<int> candidates;
+    const int max = 2000000;
+    for(int i = 2; i < max; ++i)
+        //if(!(i % 2 && i % 3 && i % 5 && i % 7))
+        if(!(i % 2 == 0) && !(i % 3 == 0) && !(i % 5 == 0) && !(i % 7 == 0)
+           && !(i % 11 == 0) && !(i % 13 == 0) && !(i % 17 == 0) && !(i % 19 == 0))
+            candidates.push_back(i);
+
+    std::cout << candidates.size() << std::endl;
 
     std::vector<int> primes = {2, 3, 5, 7, 11, 13, 17};
 
-    int currentLargePrime = primes.back();
-    while(primes.back() < 2000000)
+    auto candidate = candidates.begin();
+    while(candidate != candidates.end())
     {
-        currentLargePrime += 2;
         bool isPrime = true;
         for(auto i = primes.cbegin(); i < primes.cend(); ++i)
-            isPrime &= ((currentLargePrime % *i) != 0);
+            isPrime &= ((*candidate % *i) != 0);
         if(isPrime)
-            primes.push_back(currentLargePrime);
+        {
+            primes.push_back(*candidate);
+        }
+        candidate++;
     }
 
-    //std::cout << primes.back() << std::endl;
+    std::cout << primes.size() << std::endl;
 
     int sum;
     for(auto i = primes.cbegin(); i < primes.cend(); ++i)
