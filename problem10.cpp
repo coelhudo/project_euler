3b10761d0987c533d2e3921293578b802e1f6a6e
coeio <coeio@62d9fa40-7f12-0410-8967-a910f1bb1c9e>
2012-03-18 06:39:43 +0000

    problem 10 solved
    git-svn-id: https://svn.inf.ufsc.br/coeio/projecteuller@816 62d9fa40-7f12-0410-8967-a910f1bb1c9e

--- problem10.cpp
+++ problem10.cpp
@@ -5,12 +5,11 @@
 int main()
 {
     std::vector<int> candidates;
-    const int max = 10;
+    const int max = 2000000;
     for(int i = 2; i < max; i++)
         if(i % 2)
             candidates.push_back(i);
 
-    std::cout << candidates.size() << std::endl;
     auto next_candidate_it = candidates.begin();
     auto end = candidates.end();
 
@@ -18,29 +17,15 @@ int main()
 
     while(next_candidate_it != end)
     {
-        std::cout << "Tamanho dos candidatos: " << candidates.size() << std::endl;
-        std::cout << "Proximo candidato: " << *next_candidate_it << std::endl;
         int next_candidate = *next_candidate_it;
         end = remove_if(candidates.begin(), candidates.end(),
-                        [next_candidate](int prime){
-                            std::cout << "Candidato: " << prime << ", Next: " << next_candidate << ", " << (prime % next_candidate) << std::endl;
-                            if(!(prime % next_candidate)){
-                                std::cout << "Entrou\n";
-                                return true;
-                            }else{
-                                return false;
-                            }
-                        });
-        candidates.erase(end);
+                        [next_candidate](int prime){ return !(prime % next_candidate);});
+        candidates.erase(end, candidates.end());
         primes.push_back(next_candidate);
         next_candidate_it = candidates.begin();
-        std::for_each(candidates.cbegin(), candidates.cend(), [](int prime){ std::cout << prime << " ";});
-        std::cout << std::endl;
     }
 
-    int sum = 0;
-    std::for_each(primes.cbegin(), primes.cend(), [](int prime){ std::cout << prime << " ";});
-    std::cout << std::endl;
+    unsigned long long sum = 0;
     std::for_each(primes.cbegin(), primes.cend(), [&sum](int prime){ sum += prime;});
     std::cout << sum << std::endl;
 
