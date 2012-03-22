2c6731964b5196f87279b896df8e5bbc3f97d291
coeio <coeio@62d9fa40-7f12-0410-8967-a910f1bb1c9e>
2012-03-22 20:30:54 +0000

    trying another approach
    git-svn-id: https://svn.inf.ufsc.br/coeio/projecteuller@822 62d9fa40-7f12-0410-8967-a910f1bb1c9e

--- problem12.cpp
+++ problem12.cpp
@@ -3,20 +3,29 @@
 #include <set>
 #include <boost/lexical_cast.hpp>
 
-int findAllFactors(const int& value){
-    std::set<int> result = {1};
-    int current_factor = 2;
-    int current_value = value;
-
-    while(current_value != 1){
-        if(!(current_value % current_factor))
-            result.insert(current_factor);
-        while(!(current_value % current_factor))
+void reallyFind(std::set<unsigned long long> &result,
+                unsigned long long &current_factor,
+                unsigned long long &current_value){
+    if(!(current_value % current_factor)){
+        result.insert(current_factor);
+        do
         {
             result.insert(current_value);
             current_value = current_value/current_factor;
-        }
-        ++current_factor;
+        }while(!(current_value % current_factor));
+    }
+}
+
+int findAllFactors(const unsigned long long& value){
+    std::set<unsigned long long> result = {1};
+    unsigned long long current_factor = 2;
+    unsigned long long current_value = value;
+
+    reallyFind(result, current_factor, current_value);
+    ++current_factor;
+    while(current_value != 1){
+        reallyFind(result, current_factor, current_value);
+        current_factor += 2;
     }
 
     return result.size();
@@ -31,6 +40,8 @@ int main(int argc, char **argv){
     do{
         current = current + ++ndigits;
     }while(findAllFactors(current) < numberOfFactors);
+
+    std::cout << "N: "<< ndigits << " -> "<< current << std::endl;
 
     return 0;
 }
