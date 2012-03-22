c9e29279e39d065864e8f1490a0f3145edbe102d
coeio <coeio@62d9fa40-7f12-0410-8967-a910f1bb1c9e>
2012-03-22 00:51:24 +0000

    tentando uma nova abordagem menos custoza pra fatorar os numeros
    git-svn-id: https://svn.inf.ufsc.br/coeio/projecteuller@820 62d9fa40-7f12-0410-8967-a910f1bb1c9e

--- problem12.cpp
+++ problem12.cpp
@@ -1,53 +1,40 @@
 #include <iostream>
 #include <map>
-#include <vector>
+#include <set>
 #include <algorithm>
+#include <functional>
 
-std::map<int, int> ndigitsAndFactors;
+std::set<int> findAllFactors(const int& value){
+    std::set<int> result = {1};
+    int current_factor = 2;
+    int current_value = value;
 
-std::vector<int> findAllFactors(const int& value){
-
-    std::vector<int> result;
-    //if(value % 2 && value % 3 && value % 5)
-    // return result;
-
-    int candidate = 1;
-    //    std::cout << "old candidate: " << candidate << std::endl;
-    std::for_each(ndigitsAndFactors.begin(), ndigitsAndFactors.end(),
-                  [&candidate, value, &result](std::pair<int,int> ndigitsAndFactorPair) {
-                      if(!(value % ndigitsAndFactorPair.first)){
-                          std::cout << "Value: " << value << std::endl;
-                          std::cout << "First: " << ndigitsAndFactorPair.first << std::endl;
-                          std::cout << "Second: " << ndigitsAndFactorPair.second << std::endl;
-                      }
-                  });
- 
-    std::cout << "new candidate: " << candidate << std::endl;
-    for(; candidate < value; ++candidate)
-        if(!(value % candidate))
-            result.push_back(candidate);
-    result.push_back(value);
+    while(current_value != 1){
+        if(!(current_value % current_factor))
+            result.insert(current_factor);
+        while(!(current_value % current_factor))
+        {
+            result.insert(current_value);
+            current_value = current_value/current_factor;
+        }
+        ++current_factor;
+    }
 
     return result;
 }
 
 int main(){
-    std::vector<int> factors;
+    std::set<int> factors = {1};
     unsigned long long ndigits = 1;
     unsigned long long current = ndigits;
-    while(factors.size() < 5)
+    while(factors.size() < 100)
     {
         factors = findAllFactors(current);
-        ndigitsAndFactors[current] = factors.size();
-        std::cout << std::endl;
         current = current + ++ndigits;
     }
 
-    std::for_each(ndigitsAndFactors.begin(), ndigitsAndFactors.end(), [](std::pair<int,int> ndigitsAndFactorPair){
-            std::cout << "NDigit: " << ndigitsAndFactorPair.first <<
-                ", Factors: " << ndigitsAndFactorPair.second << std::endl;
-        });
-
+    std::for_each(factors.begin(), factors.end(), [](int value){ std::cout << value << ", ";});
+    std::cout << std::endl;
 
     std::cout << ( ndigits - 1 ) << std::endl;
     std::cout << ( current - ndigits ) << std::endl;
