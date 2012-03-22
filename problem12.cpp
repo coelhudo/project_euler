6b9ec369813ed6291117c97e23ad365ccaa7ec59
coeio <coeio@62d9fa40-7f12-0410-8967-a910f1bb1c9e>
2012-03-22 20:56:25 +0000

    rolling back the new approach to the first one
    git-svn-id: https://svn.inf.ufsc.br/coeio/projecteuller@823 62d9fa40-7f12-0410-8967-a910f1bb1c9e

--- problem12.cpp
+++ problem12.cpp
@@ -3,45 +3,32 @@
 #include <set>
 #include <boost/lexical_cast.hpp>
 
-void reallyFind(std::set<unsigned long long> &result,
-                unsigned long long &current_factor,
-                unsigned long long &current_value){
-    if(!(current_value % current_factor)){
-        result.insert(current_factor);
-        do
-        {
-            result.insert(current_value);
-            current_value = current_value/current_factor;
-        }while(!(current_value % current_factor));
-    }
-}
-
 int findAllFactors(const unsigned long long& value){
     std::set<unsigned long long> result = {1};
     unsigned long long current_factor = 2;
     unsigned long long current_value = value;
 
-    reallyFind(result, current_factor, current_value);
-    ++current_factor;
-    while(current_value != 1){
-        reallyFind(result, current_factor, current_value);
-        current_factor += 2;
-    }
+    for(int candidate = 1;candidate < value; ++candidate)
+        if(!(value % candidate))
+            result.insert(candidate);
+    result.insert(value);
 
     return result.size();
 }
 
-int main(int argc, char **argv){
-    int numberOfFactors = boost::lexical_cast<int>(argv[1]);
-
+int main(int argc, char** argv){
+    int max = boost::lexical_cast<int>(argv[1]);
     unsigned long long ndigits = 1;
     unsigned long long current = ndigits;
-
-    do{
+    int factors = 0;
+    while(factors < max)
+    {
+        factors = findAllFactors(current);
         current = current + ++ndigits;
-    }while(findAllFactors(current) < numberOfFactors);
+    }
 
-    std::cout << "N: "<< ndigits << " -> "<< current << std::endl;
+    std::cout << ( ndigits - 1 ) << std::endl;
+    std::cout << ( current - ndigits ) << std::endl;
 
     return 0;
 }
