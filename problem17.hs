4465ca9ce722229e5cbb150970137541b4d27ee5
coelhudo <coelhudo@gmail.com>
2015-04-26 23:00:03 -0400

    done

--- problem17.hs
+++ problem17.hs
@@ -3,13 +3,13 @@ import qualified Data.Map as Map
 numberDictionary = Map.fromList [(1, "one"),(2, "two"),(3, "three"),(4, "four"),(5, "five"),(6, "six"),(7, "seven"),(8, "eight"),(9, "nine"),(10, "ten"),(11, "eleven"),(12, "twelve"),(13, "thirteen"),(14, "fourteen"),(15, "fifteen"),(16, "sixteen"),(17, "seventeen"),(18, "eighteen"),(19, "nineteen"),(20, "twenty"),(30, "thirty"),(40, "forty"),(50, "fifty"),(60, "sixty"),(70, "seventy"),(80, "eighty"),(90, "ninety"),(100, "hundred"), (-1, "and")]
 
 decompose :: Int -> [Int]
-decompose x | hundredth >= 1 = [hundredth] ++ [100] ++ [-1] ++ decompose (x - (100 * hundredth))
+decompose x | hundredth >= 1 = [hundredth] ++ [100] ++ appendAnd ++ decompose (x - (100 * hundredth))
             | x `div` 20 >= 1 = [10 * tenth] ++ decompose (x - (10 * tenth))
-            | tenth >= 1 = [x]
             | x == 0 = []
             | otherwise = [x]
             where hundredth = (x `div` 100)
                   tenth = (x `div` 10)
+                  appendAnd = if x `mod` 100 == 0 then [] else [-1]
 
 numberToName :: Int -> String
 numberToName x = foldl (++) "" [extractName (Map.lookup number numberDictionary) | number <- decompose x]
