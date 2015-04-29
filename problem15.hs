c7cd26a6bf17c60c58992643b65bef80032e96c2
coelhudo <coelhudo@gmail.com>
2015-04-28 23:31:07 -0400

    problem 15 final commit. giving up :/

--- problem15.hs
+++ problem15.hs
@@ -28,23 +28,15 @@ countLeaves (Node _ left EmptyTree) = countLeaves left
 countLeaves (Node _ EmptyTree right) = countLeaves right
 countLeaves (Node _ left right) = countLeaves left + countLeaves right
 
-generateAndCount :: (Int,Int) -> (Int,Int) -> Int
-generateAndCount (x,y) (xlimit,ylimit)
-    | (x == xlimit) && (y == ylimit) = 1
-    | (x == xlimit) && (y < ylimit) = generateAndCount (xlimit, y+1) (xlimit,ylimit)
-    | (x < xlimit) && (y == ylimit) = generateAndCount (x+1,ylimit) (xlimit,ylimit)
-    | otherwise = generateAndCount (x+1,y) (xlimit,ylimit) + generateAndCount (x,y+1) (xlimit,ylimit)
-
--- Final solution - still slow but it works. There are a plenty of better solutions in the forum
-generateHalfAndCount :: (Int,Int) -> (Int,Int) -> Int -> Int
-generateHalfAndCount (x,y) (xlimit,ylimit) accu
-    | (x == xlimit) && (y == ylimit) = accu + 1
-    | (x == xlimit) && (y < ylimit) = generateHalfAndCount (xlimit, y+1) (xlimit,ylimit) accu
-    | (x < xlimit) && (y == ylimit) = generateHalfAndCount (x+1,ylimit) (xlimit,ylimit) accu
-    | (x == y) = 2 * generateHalfAndCount (x+1,y) (xlimit,ylimit) accu
-    | otherwise = generateHalfAndCount (x+1,y) (xlimit,ylimit) (accu + (generateHalfAndCount (x,y+1) (xlimit,ylimit) 0))
+--Final solution - still slow but it works. There are a plenty of better solutions in the forum
+countPossiblePathsFrom  :: (Int,Int) -> Int -> Int -> Int
+countPossiblePathsFrom  (x,y) limit accu
+    | (x == limit) && (y == limit) = accu + 1
+    | (x == limit) && (y < limit) = countPossiblePathsFrom  (limit, y+1) limit accu
+    | (x < limit) && (y == limit) = countPossiblePathsFrom  (x+1,limit) limit accu
+    | (x == y) = 2 * countPossiblePathsFrom  (x+1,y) limit accu
+    | otherwise = countPossiblePathsFrom  (x+1,y) limit (accu + (countPossiblePathsFrom  (x,y+1) limit 0))
 
 main = do
-      --print (countLeaves (generate (0,0) (14,14)))
-      print (generateHalfAndCount (0,0) (20,20) 0)
+      print (countPossiblePathsFrom  (0,0) 14 0)
 
