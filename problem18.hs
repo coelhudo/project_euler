b2fc7e77716c461c7d975e754f6bb77c891d76ab
coelhudo <coelhudo@gmail.com>
2015-04-29 22:49:23 -0400

    working now!

--- problem18.hs
+++ problem18.hs
@@ -3,13 +3,12 @@ import Control.Monad
 
 main = do
         let list = []
-        handle <- openFile "problem18_set1.txt" ReadMode
+        handle <- openFile "problem18_set2.txt" ReadMode
         contents <- hGetContents handle
         let singlelines = lines contents
             list = wordsToInt $ map words singlelines
-            path = maximumPathSum list 0 [head $ head list]
-        print path
-        print $ sum path
+            path = maximumPathSum 2 $ reverse list
+        print $ maximum path
         hClose handle
 
 wordsToInt :: [[String]] -> [[Int]]
@@ -18,9 +17,10 @@ wordsToInt = map wordToInt
 wordToInt :: [String] -> [Int]
 wordToInt = map read
 
-maximumPathSum :: [[Int]] -> Int -> [Int] -> [Int]
-maximumPathSum (x:[]) column accumulator = accumulator
-maximumPathSum (x:y:xs) column accumulator = maximumPathSum (y:xs) findNextColumn (accumulator ++ [maxValue])
-                where  maxValue = max (y !! column) (y !! nextColumn)
-                       findNextColumn = if (y !! column) < (y !! nextColumn) then nextColumn else column
-                       nextColumn = succ column
+combinea :: [Int] -> [Int] -> Int -> [Int]
+combinea xs [] current = []
+combinea xs ys current = map (+ head ys) (take current xs) ++ (combinea (drop (current `div` 2) xs) (tail ys) current)
+
+maximumPathSum :: Int -> [[Int]] -> [Int]
+maximumPathSum current (xs:[]) = xs
+maximumPathSum current (xs:ys:xxs) = maximumPathSum (2 * current) $ combinea xs ys current : xxs
